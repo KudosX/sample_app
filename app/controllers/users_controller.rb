f2afd0abb7fa58d3bd7a3c0c33c847df4942ce50
KudosX <max@shitbrains.com>
2016-02-18 14:19:15 -0800

    finished login/logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       # Handle a successful save.
       flash[:success] = "Welcome all yee Minions to my Unicorn Odyssey!"
       redirect_to @user
