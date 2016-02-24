3c4655538232fd59f9bcef2a855c7f3b9fc2ab36
KudosX <max@shitbrains.com>
2016-02-24 10:03:46 -0800

    add account activations

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,10 @@
 User.create!(name:  "Example User",
              email: "example@example.com",
              password:              "foobar",
-             password_confirmation: "foobar", admin: true)
+             password_confirmation: "foobar",
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 
 99.times do |n|
